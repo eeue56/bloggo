3ec2575933b8a4a26b4d1e46a2e7547ab7fbd88d
eeue56 <eeue56@bangor.ac.uk>
2013-08-07 20:13:31 +0000

    adding basic login/logout

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,36 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+    "github.com/robfig/revel"
+    "bloggo/app/models"
+)
 
 type App struct {
 	*revel.Controller
 }
 
+func (c App) Login(user *models.User) revel.Result {
+    user.Validate(c.Validation)
+
+    if c.Validation.HasErrors() {
+        c.Validation.Keep()
+        c.FlashParams()
+        return c.Redirect("/")
+    }
+
+    c.Session["logged_in"] = "true";
+
+    return c.Redirect("/") 
+}
+
+func (c App) Logout() revel.Result {
+    c.Session["logged_in"] = "";
+
+    return c.Redirect("/") 
+}
+
 func (c App) Index() revel.Result {
+    c.RenderArgs["latest"] = models.GetLatestPost();
+    c.RenderArgs["logged_in"] = c.Session["logged_in"]
 	return c.Render()
 }
